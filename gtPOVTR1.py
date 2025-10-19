add911fe1f4c17efefde39f4a8e6815a4d2a5f22
Daniyar Azhgaliev <tasherno1@gmail.com>
2025-10-19 12:28:22 +0500

    exem

---
+++ gtPOVTR1.py
@@ -0,0 +1,34 @@
+# Задание 1.
+# text = input("Введите строку: ")
+
+# clean_text = text.replace(" ", "").lower()
+# reversed_text = ""
+
+# for ch in clean_text:
+#     reversed_text = ch + reversed_text
+
+# if clean_text == reversed_text:
+#     print("YES")
+# else:
+#     print("NO")
+
+#Задание 2.
+text = input("Введите строку со скобками: ")
+
+stack = []
+pairs = {')': '(', ']': '[', '}': '{'}
+ok = True
+
+for ch in text:
+    if ch in "([{":
+        stack.append(ch)
+    elif ch in ")]}":
+        if not stack or stack[-1] != pairs[ch]:
+            ok = False
+            break
+        stack.pop()
+
+if ok and not stack:
+    print("YES")
+else:
+    print("NO")
