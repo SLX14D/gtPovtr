1f5e2b2edf4cbf4b7fa95d946c163aaa4ba6a30c
Daniyar Azhgaliev <tasherno1@gmail.com>
2025-11-02 13:39:11 +0500

    c2

---
+++ openCV.py
@@ -0,0 +1,25 @@
+import cv2
+from PIL import Image
+
+cat_face_cascade = cv2.CascadeClassifier('haarcascade_frontalcatface_extended.xml')
+
+cap = cv2.VideoCapture(0)  
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    cat_faces = cat_face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
+
+    for (x, y, w, h) in cat_faces:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+    cv2.imshow("Cat Detector", frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
